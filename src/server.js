3ae38197ea8f99340efa82babd3abc58481c145e
Pedro Henrique <pedro.progsantos@gmail.com>
2025-07-02 03:04:31 -0300

    Refatoração do teste de rotas;

--- src/server.js
+++ src/server.js
@@ -1,11 +1,3 @@
-const express = require('express');
+const app = require('./app');
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.status(200).send();
-});
-
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-});
+app.listen(3000);
--- test/server.test.js
+++
@@ -1,8 +0,0 @@
-const supertest = require('supertest');
-const request = supertest('http://localhost:3000');
-
-test('Deve responder na porta 3000', async () => {
-  return request.get('/').then((response) => {
-    expect(response.status).toBe(200);
-  });
-});
