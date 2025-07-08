3a3d9bd282021dd275b92c91c971e0f9daff37db
Pedro Henrique <pedro.progsantos@gmail.com>
2025-07-08 10:32:06 -0300

    Protegendo rotas com passport

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,17 +1,23 @@
 module.exports = (app) => {
+  app.route('/auth/signin')
+    .post(app.routes.auth.signin);
+
+  app.route('/auth/signup')
+    .post(app.routes.users.create);
+
   app.route('/users')
+    .all(app.config.passport.authenticate())
     .get(app.routes.users.findAll)
     .post(app.routes.users.create);
 
   app.route('/accounts')
+    .all(app.config.passport.authenticate())
     .get(app.routes.account.findAll)
     .post(app.routes.account.create);
 
   app.route('/accounts/:id')
+    .all(app.config.passport.authenticate())
     .get(app.routes.account.findById)
     .delete(app.routes.account.removeById)
     .put(app.routes.account.updateById);
-
-  app.route('/auth/signin')
-    .post(app.routes.auth.signin)
 }
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,7 +1,18 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
-const MAIN_ROUTE = '/auth/signin'
+const MAIN_ROUTE = '/auth'
+
+test('Deve criar um usuario via signup', () => {
+  return request(app).post(`${MAIN_ROUTE}/signup`)
+    .send({ name: 'Kaido das 100 feras', email: `kaido_${Date.now()}@gmail.com`, password: 'teste132456' })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Kaido das 100 feras')
+      expect(res.body).toHaveProperty('email');
+      expect(res.body).not.toHaveProperty('password');
+    });
+});
 
 test('Deve receber um token ao logar', async () => {
   const email = `crocoboy${Date.now()}@mail.com`;
@@ -11,7 +22,7 @@ test('Deve receber um token ao logar', async () => {
     password: 'teste123'
   });
 
-  const res = await request(app).post(MAIN_ROUTE)
+  const res = await request(app).post(`${MAIN_ROUTE}/signin`)
     .send({ email, password: 'teste123' });
 
   expect(res.status).toBe(200);
@@ -26,7 +37,7 @@ test('Não deve autenticar usuario com senha errada', async () => {
     password: 'teste123'
   });
 
-  const res = await request(app).post(MAIN_ROUTE)
+  const res = await request(app).post(`${MAIN_ROUTE}/signin`)
     .send({ email, password: '123teste' });
 
   expect(res.status).toBe(400);
@@ -34,9 +45,16 @@ test('Não deve autenticar usuario com senha errada', async () => {
 });
 
 test('Não deve autenticar usuario que não existe', async () => {
-  const res = await request(app).post(MAIN_ROUTE)
+  const res = await request(app).post(`${MAIN_ROUTE}/signin`)
     .send({ email: 'teste@mail.com', password: '123teste' });
 
   expect(res.status).toBe(400);
   expect(res.body).toHaveProperty('error', 'Email ou senha invalido');
+});
+
+test('Não deve acessar uma rota protegida sem token', () => {
+  return request(app).get('/users')
+    .then((res) => {
+      expect(res.status).toBe(401);
+    })
 });
